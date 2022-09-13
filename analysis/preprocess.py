8dcacf04a5956ee4c77e22649afa552a7f5d02ef
known5 <lucjansen62@gmail.com>
2022-09-13 09:05:17 +0200

    improved preprocess functions

--- analysis/preprocess.py
+++ analysis/preprocess.py
@@ -1,20 +1,33 @@
 import pandas as pd
 
 ########################################################################################################################
+# # Variables
+# filepath = '../log/Attack/active/no_overlap/575_active_attack_25.txt'
+# filepath1 = '../log/Attack/active/no_overlap/577_active_attack_42.txt'
+# filepath2 = '../log/Attack/active/no_overlap/579_active_attack_56.txt'
+# filepath3 = '../log/Attack/active/no_overlap/572_active_attack_78.txt'
+# filepath4 = '../log/Attack/active/no_overlap/576_active_attack_92.txt'
+
 # Variables
+filepath = '../log/Attack/passive/no_overlap/536_passive_attack_25.txt'
+filepath1 = '../log/Attack/passive/no_overlap/560_passive_attack_42.txt'
+filepath2 = '../log/Attack/passive/no_overlap/571_passive_attack_56.txt'
+filepath3 = '../log/Attack/passive/no_overlap/540_passive_attack_78.txt'
+filepath4 = '../log/Attack/passive/no_overlap/535_passive_attack_92.txt'
 
-
-filepath = '../log/Attack/active/599_active_attack_92.txt'
-data = pd.read_csv(filepath, delimiter='INFO:root:', on_bad_lines='skip', engine='python')
+paths = [('25', filepath),
+         ('42', filepath1),
+         ('56', filepath2),
+         ('78', filepath3),
+         ('92', filepath4)]
 
 pd.options.display.width = None
 pd.options.display.max_columns = None
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
 
-########################################################################################################################
 
-if __name__ == "__main__":
+def convert_dataframe(data):
     # Fix cell values for all rows and columns
     data = data.drop(columns=['Unnamed: 0'])
     temp = data.columns[0]
@@ -29,6 +42,7 @@ if __name__ == "__main__":
     data = data.replace('Time: ', '', regex=True)
     data = data.replace('Loss: ', '', regex=True)
     data = data.replace('Acc: ', '', regex=True)
+    data = data.replace('Avg ', '', regex=True)
     data = data.replace('Conf Matrix: ', '', regex=True)
     data = data.replace(' \]', '', regex=True)
     data = data.replace('TP:', '', regex=True)
@@ -36,6 +50,7 @@ if __name__ == "__main__":
     data = data.replace('TN:', '', regex=True)
     data = data.replace('FN:', '', regex=True)
     data = data.replace('\%', '', regex=True)
+
     data['4'] = data['4'].replace('s', '', regex=True)
     temp = data['6'].str.split(' ', 4, expand=True)
     data = data.join(temp)
@@ -47,11 +62,32 @@ if __name__ == "__main__":
 
     data = data[(data['Round'].str.contains('Totals'))]
     data = data.replace('Totals ', '', regex=True)
+    data = data.replace('Total ', '', regex=True)
+    data = data.replace('Attacker ', '', regex=True)
+    data['Time'] = data['Time'].replace('s', '', regex=True)
     data = data.astype({'Accuracy': 'double'})
+    data = data.astype({'Loss': 'double'})
+    data = data.astype({'Time': 'double'})
     data = data.astype({'Round': 'int'})
+
+    data = data.astype({'True P': 'int'})
+    data = data.astype({'False P': 'int'})
+    data = data.astype({'True N': 'int'})
+    data = data.astype({'False N': 'int'})
 
     temp1 = data[(data['Type'].str.contains('Train'))]
     temp2 = data[(data['Type'].str.contains('Test'))]
 
-    print(temp1.loc[temp1.Accuracy.idxmax()])
-    print(temp2.loc[temp2.Accuracy.idxmax()])
+    return pd.merge(temp1, temp2, on='Round')
+
+
+########################################################################################################################
+
+if __name__ == "__main__":
+
+    with pd.ExcelWriter('../log/excel/passive_attack_no_overlap.xlsx') as writer:
+        for path in paths:
+            seed, file = path
+            data = pd.read_csv(file, delimiter='INFO:root:', on_bad_lines='skip', engine='python', decimal=',')
+            result = convert_dataframe(data)
+            result.to_excel(excel_writer=writer, sheet_name=seed)
--- analysis/preprocess_target_training.py
+++ analysis/preprocess_target_training.py
@@ -1,21 +1,44 @@
+import numpy as np
 import pandas as pd
 
 ########################################################################################################################
 # Variables
 
+# # Variables
+# filepath = '../log/Target/Target_train_client_4/299_Target_training_clients_4_rounds_300_batch_64_lr_0' \
+#            '.01_overlap_yes_overlapsize_30000.txt'
+# filepath1 = '../log/Target/Target_train_client_4/295_Target_training_clients_4_rounds_300_batch_64_lr_0' \
+#            '.01_overlap_yes_overlapsize_30000.txt'
+# filepath2 = '../log/Target/Target_train_client_4/296_Target_training_clients_4_rounds_300_batch_64_lr_0' \
+#            '.01_overlap_yes_overlapsize_30000.txt'
+# filepath3 = '../log/Target/Target_train_client_4/297_Target_training_clients_4_rounds_300_batch_64_lr_0' \
+#            '.01_overlap_yes_overlapsize_30000.txt'
+# filepath4 = '../log/Target/Target_train_client_4/298_Target_training_clients_4_rounds_300_batch_64_lr_0' \
+#            '.01_overlap_yes_overlapsize_30000.txt'
 
-filepath = '../log/Target/Target_train_client_4/298_Target_training_clients_4_rounds_300_batch_64_lr_0' \
-           '.01_overlap_yes_overlapsize_30000.txt '
-data = pd.read_csv(filepath, delimiter='INFO:root:', on_bad_lines='skip', engine='python')
+
+# Variables
+filepath = '../log/Target/active/epoch_1/461_active_target_training.txt'
+filepath1 = '../log/Target/active/epoch_1/464_active_target_training.txt'
+filepath2 = '../log/Target/active/epoch_1/460_active_target_training.txt'
+filepath3 = '../log/Target/active/epoch_1/462_active_target_training.txt'
+filepath4 = '../log/Target/active/epoch_1/458_active_target_training.txt'
+
+paths = [('25', filepath),
+         ('42', filepath1),
+         ('56', filepath2),
+         ('78', filepath3),
+         ('92', filepath4)]
 
 pd.options.display.width = None
 pd.options.display.max_columns = None
 pd.set_option('display.max_rows', 3000)
 pd.set_option('display.max_columns', 3000)
 
+
 ########################################################################################################################
 
-if __name__ == "__main__":
+def convert_dataframe(data):
     # Fix cell values for all rows and columns
     data = data.drop(columns=['Unnamed: 0'])
     data = data.iloc[1:]
@@ -27,17 +50,66 @@ if __name__ == "__main__":
     data = data.replace('\[ Round: ', '', regex=True)
     data = data.replace('Client: ', '', regex=True)
     data = data.replace('Batch ', '', regex=True)
+    data = data.replace('Local ', '', regex=True)
     data = data.replace('Time: ', '', regex=True)
     data = data.replace('Loss: ', '', regex=True)
     data = data.replace('Tr_Acc: ', '', regex=True)
     data = data.replace(' \]', '', regex=True)
+    data = data.replace('%', '', regex=True)
 
-    data['4'] = data['4'].replace('s', '', regex=True)
-    temp = data['6'].str.split(' ', 4, expand=True)
-    data = data.join(temp)
-    data = data.drop(columns=['6', 0])
+    data = data[data['2'].str.contains('!') == False]
+    data = data[data['2'].str.contains('Global Model Eval started') == False]
+    data['6'] = data['6'].replace(r'^.{0,20}', '', regex=True)
+    data = data.astype({'1': 'int'})
+    temp1 = data[data['1'] <= 2]
+    temp2 = data[data['1'] > 2]
+    temp3 = temp2[temp2['1'] > 6]
+    temp2 = temp2[temp2['1'] <= 6]
+    temp4 = temp3[temp3['1'] <= 21]
+    temp5 = temp3[temp3['1'] > 21]
 
-    data.columns = ['Round', 'Type', 'Time', 'Loss', 'Accuracy', "temp1", "temp2"]
-    data = data[data['Time'].notna()]
-    data = data[data['Loss'].str.contains('Accuracy')]
-    print(data)
+    temp2['6'] = temp2['6'].replace(r'^.{0,1}', '', regex=True)
+    temp4['6'] = temp4['6'].replace(r'^.{0,1}', '', regex=True)
+    temp5['6'] = temp5['6'].replace(r'^.{0,2}', '', regex=True)
+    data = pd.concat([temp1, temp2, temp4, temp5])
+
+    temp1 = data[data['2'].str.contains('s')]
+    data = data[data['2'].str.contains('s') == False]
+    temp1.insert(1, '8', 'Global')
+    temp1.insert(1, '7', 'Eval')
+    temp1 = temp1.drop(columns=['5', '6'])
+    temp1.columns = ['1', '2', '3', '4', '5', '6']
+    temp2 = temp1[temp1['1'] <= 5]
+    temp3 = temp1[temp1['1'] > 5]
+    temp2['6'] = temp2['6'].replace(r'^.{0,23}', '', regex=True)
+    temp3['6'] = temp3['6'].replace(r'^.{0,24}', '', regex=True)
+    temp1 = pd.concat([temp2, temp3])
+
+    temp1.columns = ['Round', 'Type', 'ID', 'Time', 'Loss', 'Accuracy']
+    temp1['Time'] = temp1['Time'].replace('s', '', regex=True)
+    temp1 = temp1.astype({'Loss': 'double'})
+    temp1 = temp1.astype({'Time': 'double'})
+
+    data.columns = ['Round', 'Type', 'ID', 'Time', 'Loss', 'Accuracy']
+    data['Time'] = data['Time'].replace('s', '', regex=True)
+    data = data.astype({'Accuracy': 'double'})
+    data = data.astype({'Loss': 'double'})
+    data = data.astype({'Time': 'double'})
+    data = data.astype({'Round': 'int'})
+    data = data.groupby(np.arange(len(data.index)) // 4).mean()
+
+    result = pd.merge(data, temp1, on='Round')
+    result = result.drop(columns=['ID'])
+    result = result.replace('=', '', regex=True)
+    result = result.astype({'Accuracy_y': 'double'})
+    return result
+
+
+if __name__ == "__main__":
+
+    with pd.ExcelWriter('../log/excel/target_training.xlsx') as writer:
+        for path in paths:
+            seed, file = path
+            data = pd.read_csv(file, delimiter='INFO:root:', on_bad_lines='skip', engine='python', decimal=',')
+            result = convert_dataframe(data)
+            result.to_excel(excel_writer=writer, sheet_name=seed)
